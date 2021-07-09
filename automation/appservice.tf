615508337a15d6ffd2e3abb746cb08288d52c923
Willian Romao <willian.romao@outlook.com.br>
2021-07-09 01:04:59 -0300

    criacao do webapp hml AB#16

--- automation/appservice.tf
+++ automation/appservice.tf
@@ -29,8 +29,4 @@ resource "azurerm_app_service" "MobEADv2-plan-hml" {
   resource_group_name = "AzureDevOps"
   app_service_plan_id = "${azurerm_app_service_plan.MobEADv2-plan-hml.id}"
 
-  site_config {
-    dotnet_core_version = "v3.1"
-  }
-
 }
