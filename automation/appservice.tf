a5b03816648ff383740f302fe2b2e6e9d6ca5533
Willian Romao <willian.romao@outlook.com.br>
2021-07-09 12:41:18 -0300

    criacao do webapp prd AB#16

--- automation/appservice.tf
+++ automation/appservice.tf
@@ -31,3 +31,25 @@ resource "azurerm_app_service" "MobEADv2-hml" {
   app_service_plan_id = "${azurerm_app_service_plan.MobEADv2-plan-hml.id}"
 
 }
+
+resource "azurerm_app_service_plan" "MobEADv2-plan-prd" {
+  name                = "MobEADv2-plan-prd"
+  location            = "East US"
+  resource_group_name = "AzureDevOps"
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+
+}
+
+resource "azurerm_app_service" "MobEADv2-prd" {
+  name                = "mobeadv2-prd"
+  location            = "East US"
+  resource_group_name = "AzureDevOps"
+  app_service_plan_id = "${azurerm_app_service_plan.MobEADv2-plan-prd.id}"
+
+}
